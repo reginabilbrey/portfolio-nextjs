664bc63cbde92e1155cf47bda6fa452ffcd904fe
reginabilbrey <regbilbrey@gmail.com>
2026-05-11 13:16:06 -0700

    robots.txt, layout.tsx, sitemap.ts, google search console

---
+++ nextjs/app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'http://localhost:3000',
+      lastModified: new Date(),
+    },
+    {
+      url: 'http://localhost:3000/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'http://localhost:3000/contact',
+      lastModified: new Date(),
+    },
+  ]
+} 
